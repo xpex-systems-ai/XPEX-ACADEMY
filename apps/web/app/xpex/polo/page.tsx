923d97008102c251aedccfbe9bf7b4090299002d
JUNIOR SENA <xpexsena2@gmail.com>
2026-09-02 18:53:04 -0300

    fix(auth): force dynamic polo entry

--- apps/web/app/xpex/polo/page.tsx
+++ apps/web/app/xpex/polo/page.tsx
@@ -1,5 +1,8 @@
 import { AuthenticatedXpexExperience } from '@components/Xpex/AuthenticatedXpexExperience'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default function XpexPoloPage() {
   return <AuthenticatedXpexExperience requestedRole="professora" returnPath="/xpex/polo" />
 }
