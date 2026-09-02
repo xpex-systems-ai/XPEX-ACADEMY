e21ca480b7ed9dc10682324261b578d8d0a288a8
JUNIOR SENA <xpexsena2@gmail.com>
2026-09-02 17:14:19 -0300

    fix(auth): force dynamic XPeX entry

--- apps/web/app/xpex/page.tsx
+++ apps/web/app/xpex/page.tsx
@@ -1,3 +1,6 @@
 import { AuthenticatedXpexExperience } from '@components/Xpex/AuthenticatedXpexExperience'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default function XpexExperiencePage() { return <AuthenticatedXpexExperience returnPath="/xpex" /> }
