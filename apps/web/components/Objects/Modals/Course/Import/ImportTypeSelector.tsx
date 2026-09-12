ffe072700865995d23e0d1a05bf2e3a5c32d13c6
JUNIOR SENA <xpexsena2@gmail.com>
2026-09-11 23:18:11 -0300

    fix(xpex): clean containment import selector lint

--- apps/web/components/Objects/Modals/Course/Import/ImportTypeSelector.tsx
+++ apps/web/components/Objects/Modals/Course/Import/ImportTypeSelector.tsx
@@ -1,5 +1,4 @@
 'use client'
-import React from 'react'
 import { GraduationCap } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
 import PlanBadge from '@components/Dashboard/Shared/PlanRestricted/PlanBadge'
