7430002f58d7ba35d1cdea825a58b2ae969282f7
JUNIOR SENA <xpexsena2@gmail.com>
2026-09-11 23:08:54 -0300

    feat(xpex): replace legacy dashboard chrome with XpeX containment shell

--- apps/web/app/orgs/[orgslug]/dash/ClientAdminLayout.tsx
+++ apps/web/app/orgs/[orgslug]/dash/ClientAdminLayout.tsx
@@ -1,42 +1,33 @@
-'use client';
-import DashLeftMenu from '@components/Dashboard/Menus/DashLeftMenu';
-import DashMobileMenu from '@components/Dashboard/Menus/DashMobileMenu';
-import OnboardingTracker from '@components/Dashboard/Onboarding/OnboardingTracker';
-import WelcomeModal from '@components/Dashboard/Onboarding/WelcomeModal';
+'use client'
+
 import AdminAuthorization from '@components/Security/AdminAuthorization'
 import { SessionGate } from '@components/Contexts/LHSessionContext'
 import { CommandPaletteProvider } from '@components/Dashboard/CommandPalette/CommandPaletteContext'
 import CommandPalette from '@components/Dashboard/CommandPalette/CommandPalette'
 import React from 'react'
-import { useMediaQuery } from 'usehooks-ts';
+import XpexNativeAdminMenu from './XpexNativeAdminMenu'
 
 function ClientAdminLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
-    // Accepted (the server layout passes it) but unused here.
-    params?: any
+  children: React.ReactNode
+  params?: any
 }) {
-    const isMobile = useMediaQuery('(max-width: 1024px)')
-
-    return (
-        <SessionGate>
-            <AdminAuthorization authorizationMode="page">
-                <CommandPaletteProvider>
-                    {isMobile && <DashMobileMenu />}
-                    <div className="flex flex-col lg:flex-row">
-                        {!isMobile && <DashLeftMenu />}
-                        <div className="flex flex-col w-full min-w-0 relative isolate pb-24 lg:pb-0">
-                            {children}
-                            <OnboardingTracker />
-                        </div>
-                        <WelcomeModal />
-                        <CommandPalette />
-                    </div>
-                </CommandPaletteProvider>
-            </AdminAuthorization>
-        </SessionGate>
-    )
+  return (
+    <SessionGate>
+      <AdminAuthorization authorizationMode="page">
+        <CommandPaletteProvider>
+          <div className="xpex-native-admin">
+            <XpexNativeAdminMenu />
+            <div className="xpex-native-admin-main">
+              {children}
+            </div>
+            <CommandPalette />
+          </div>
+        </CommandPaletteProvider>
+      </AdminAuthorization>
+    </SessionGate>
+  )
 }
 
 export default ClientAdminLayout
